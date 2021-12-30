59afeff11661572a5c2dca0e312958f886d0ede5
Gustavo Mota <gustavomota@MacBook-Air-de-Gustavo.local>
2021-12-29 21:28:36 -0300

    Atualizacoes

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO veiculo (placa, marca, modelo) VALUES
+  ('DSQ-4747', 'Honda', 'Civic'),
+  ('EZG-7498', 'Volkswagem', 'Fox');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE veiculo (
+  placa VARCHAR(32) PRIMARY KEY,
+  marca VARCHAR(32),
+  modelo VARCHAR(32)
+);
